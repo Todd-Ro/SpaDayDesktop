4b2b69a24402d653162e0106e58b0f0116b09078
Todd Romkema <todd.romkema@gmail.com>
2022-01-09 20:46:09 -0600

    Finished large portion of Studio 14 with the UserController

---
+++ SpaDayDesktop/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDayDesktop.Models;
+
+namespace SpaDayDesktop.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser)
+        {
+            ViewBag.NewUser = newUser;
+            return View("Index");
+        }
+
+    }
+}
